234eb830732b5fb409a2e720b5962155ada8baa8
LatIv18 <im.lattanzio@gmail.com>
2024-05-21 18:20:35 -0300

    arregle nav y sus links, tambien puse generos correctos en seccion Index

--- Static/js/alerta.js
+++ Static/js/alerta.js
@@ -2,3 +2,7 @@ document.addEventListener(onload,
     alert("Esto es solo una página de muestra, al presionar en comprar no lleva a ningún lado.")
     
 )
+
+document.getElementById('buy-btn').onclick = function(){
+    alert("Esto es solo una página de muestra, al presionar en comprar no lleva a ningún lado.");
+}
